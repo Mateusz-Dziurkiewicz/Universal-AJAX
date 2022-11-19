a02ccafd91822bf4ddc02889763af274e08a8668
Mateusz Dziurkiewicz <17400359+Mateusz-Dziurkiewicz@users.noreply.github.com>
2022-11-19 11:30:34 +0000

    Rewrite (Breaking Changes) (#11)
    * JSDoc Fixes
    
    * Updated README.md
    
    * added minified variant of uajax.js, updated readme
    
    * Fixed typos
    
    * Added ability to specify contentType and dataType
    
    * Fixed typo
    
    * Fixed bugs. Fixed typo. Renamed Response class to UAJAXResponse to avoid conflicts with other libraries.
    
    * Minifed for release
    
    * Fixed spelling error
    
    * Fixed upload method
    
    * Added crossdomain option
    
    * Updated README. Introduced BREAKING CHANGES - generally everything is now less shit, with much better option and arg handling. Also implemented everything with an actual linter - it's 2022 for crying out loud!

--- uajax.js
+++ uajax.js
@@ -1,169 +1,37 @@
+/* eslint-disable no-console */
+
 /**
  * UAJAX
  * @description A universal AJAX module.
  * @author Mateusz Dziurkiewicz
- *
- * @param {String} method The HTTP method to use
- * @param {String} path API Path. e.g. /api/v1/my-account
- * @param {Object} data Optional payload
- * @param {Object} headers Optional request headers
- *
- * @returns {Promise} API Response
- * @example const response = await uajax('get', '/api/v1/my-account/social');
  */
 
-uajax_debug = true;
+const uajaxDebug = true;
 
-uajax = async (method, path, data = {}, headers = {}, contentType = 'application/json', dataType = 'json', crossDomain = false) => {
-	// If uajax_debug is enabled, we'll output what the module is doing to the console.
-	let tx = 'color: rgb(0, 255, 145)';
-	let rx = 'color: rgb(52, 164, 250)';
-
-	if (method.toUpperCase() == 'GET') {
-		if (uajax_debug) {
-			console.log('GET %c>>>', tx, path);
-		}
-
-		return new Promise((resolve) => {
-			$.ajax({
-				method: 'GET',
-				url: path,
-				contentType: contentType,
-				crossDomain: crossDomain,
-				dataType: dataType,
-				headers: headers,
-
-				success: (r, textStatus, xhr) => {
-					let response = new UAJAXResponse(r, xhr.status);
-
-					if (uajax_debug) {
-						console.log('GET %c<<<', rx, path, response);
-					}
-					resolve(response);
-				},
-			}).catch((xhr) => {
-				const error_details = {
-					status: xhr.status,
-					response: xhr.responseText,
-				};
-
-				if (uajax_debug) {
-					console.error(`${method.toUpperCase()} to ${path} encountered an exception.`, error_details);
-				}
-
-				let response = new UAJAXResponse(error_details['response'], error_details['status']);
-				resolve(response);
-			});
-		});
-	} else {
-		if (uajax_debug) {
-			console.log(`${method.toUpperCase()} %c>>>`, tx, path, data);
-		}
-
-		return new Promise((resolve) => {
-			$.ajax({
-				method: method.toUpperCase(),
-				url: path,
-				data: JSON.stringify(data),
-				contentType: contentType,
-				crossDomain: crossDomain,
-				dataType: dataType,
-				headers: headers,
-
-				success: (r, textStatus, xhr) => {
-					let response = new UAJAXResponse(r, xhr.status);
-
-					if (uajax_debug) {
-						console.log(`${method.toUpperCase()} %c<<<`, rx, path, response);
-					}
-					resolve(response);
-				},
-			}).catch((xhr) => {
-				const error_details = {
-					status: xhr.status,
-					response: xhr.responseText,
-				};
-
-				if (uajax_debug) {
-					console.error(`${method.toUpperCase()} to ${path} encountered an exception.`, error_details);
-				}
-
-				let response = new UAJAXResponse(error_details['response'], error_details['status']);
-				resolve(response);
-			});
-		});
-	}
+const defaults = {
+  data: {},
+  headers: {},
+  contentType: 'application/json',
+  dataType: 'json',
+  crossDomain: false,
 };
 
-uajax.upload = (path, formData, progress = false, progressBarID = '', crossDomain = false) => {
-	// If uajax_debug is enabled, we'll output what the module is doing to the console.
-	let tx = 'color: rgb(0, 255, 145';
-	let rx = 'color: rgb(52, 164, 250)';
+/**
+ * Process options and apply defaults to anything that is missing.
+ * @param {Object} options - Initialize options object, or apply defaults
+ * @returns {Object} Request options
+ */
+const applyDefaultOptions = (options) => {
+  // Apply defaults if options are not provided
+  const initializedOptions = options;
 
-	if (uajax_debug) {
-		console.log('POST (File Upload) %c>>>', tx, path, formData);
-	}
+  if (Object.keys(initializedOptions).length < Object.keys(defaults).length) {
+    Object.entries(defaults).forEach(([key, value]) => {
+      initializedOptions[key] = initializedOptions[key] ?? value;
+    });
+  }
 
-	return new Promise((resolve) => {
-		$.ajax({
-			xhr: () => {
-				let xhr = new window.XMLHttpRequest();
-
-				xhr.upload.addEventListener(
-					'progress',
-					function (evnt) {
-						if (evnt.lengthComputable) {
-							let progressPercentage = evnt.loaded / evnt.total;
-							progressPercentage = parseInt(progressPercentage * 100);
-						}
-
-						if (progress) {
-							$(`#${progressBarID}`).css({ width: `${progressPercentage}%` });
-							$(`#${progressBarID}`).html(`${progressPercentage}%`);
-						}
-					},
-					false
-				);
-
-				return xhr;
-			},
-
-			method: 'POST',
-			url: path,
-			data: formData,
-			cache: false,
-			contentType: false,
-			crossDomain: crossDomain,
-			processData: false,
-			dataType: 'json',
-
-			success: (r, textStatus, xhr) => {
-				let response = new UAJAXResponse(r, xhr.status);
-
-				if (uajax_debug) {
-					console.log('POST (File Upload) %c<<<', rx, path, response);
-				}
-
-				if (progress) {
-					$(`#${progressBarID}`).html('Upload completed!');
-				}
-
-				resolve(response);
-			},
-		}).catch((xhr) => {
-			const error_details = {
-				status: xhr.status,
-				response: xhr.responseText,
-			};
-
-			if (uajax_debug) {
-				console.error(`Failed to upload file to ${path}`, error_details);
-			}
-
-			let response = new UAJAXResponse(error_details['response'], error_details['status']);
-			resolve(response);
-		});
-	});
+  return initializedOptions;
 };
 
 /**
@@ -174,12 +42,183 @@ uajax.upload = (path, formData, progress = false, progressBarID = '', crossDomai
  * @param {xhr} The XHR Response
  * @param {xhr} The XHR Status Code
  *
- * @returns {Object} The Response object.
+ * @returns {Object} The Response object
  * @example let response = new UAJAXResponse(r, xhr.status);
  */
 class UAJAXResponse {
-	constructor(body, status) {
-		this.body = body;
-		this.status = status;
-	}
+  constructor(body, status) {
+    this.body = body;
+    this.status = status;
+  }
 }
+
+/**
+ * Universal AJAX Method
+ * @param {String} method - Request method
+ * @param {String} path - Request path
+ * @param {Object} options - Request options
+ * @returns Promise
+ */
+const uajax = async (method, path, { ...options } = {}) => {
+  const tx = 'color: rgb(0, 255, 145)';
+  const rx = 'color: rgb(52, 164, 250)';
+
+  const initializedOptions = applyDefaultOptions(options);
+
+  if (method.toUpperCase() === 'GET') {
+    if (uajaxDebug) {
+      console.log('GET %c>>>', tx, path);
+    }
+
+    return new Promise((resolve) => {
+      $.ajax({
+        method: 'GET',
+        url: path,
+        contentType: options.contentType,
+        crossDomain: options.crossDomain,
+        dataType: options.dataType,
+        headers: options.headers,
+
+        success: (r, textStatus, xhr) => {
+          const response = new UAJAXResponse(r, xhr.status);
+
+          if (uajaxDebug) {
+            console.log('GET %c<<<', rx, path, response);
+          }
+          resolve(response);
+        },
+      }).catch((xhr) => {
+        const errorDetails = {
+          status: xhr.status,
+          response: xhr.responseText,
+        };
+
+        if (uajaxDebug) {
+          console.error(`${method.toUpperCase()} to ${path} encountered an exception.`, errorDetails);
+        }
+
+        const response = new UAJAXResponse(errorDetails.response, errorDetails.status);
+        resolve(response);
+      });
+    });
+  }
+  if (uajaxDebug) {
+    console.log(`${method.toUpperCase()} %c>>>`, tx, path, options.data);
+  }
+
+  return new Promise((resolve) => {
+    $.ajax({
+      method: method.toUpperCase(),
+      url: path,
+      data: JSON.stringify(options.data),
+      contentType: initializedOptions.contentType,
+      crossDomain: initializedOptions.crossDomain,
+      dataType: initializedOptions.dataType,
+      headers: initializedOptions.headers,
+
+      success: (r, textStatus, xhr) => {
+        const response = new UAJAXResponse(r, xhr.status);
+
+        if (uajaxDebug) {
+          console.log(`${method.toUpperCase()} %c<<<`, rx, path, response);
+        }
+        resolve(response);
+      },
+    }).catch((xhr) => {
+      const errorDetails = {
+        status: xhr.status,
+        response: xhr.responseText,
+      };
+
+      if (uajaxDebug) {
+        console.error(`${method.toUpperCase()} to ${path} encountered an exception.`, errorDetails);
+      }
+
+      const response = new UAJAXResponse(errorDetails.response, errorDetails.status);
+      resolve(response);
+    });
+  });
+};
+
+/**
+ * File upload method.
+ * @param {String} method - Request method
+ * @param {FormData} formData - FormData (such as a file)
+ * @param {Boolean} progress - Set to true if progress is to be calculated. Requires progressBarID
+ * @param {Boolean} progressBarID - ID of progress bar
+ * @param {Object} options - Request options
+ * @returns Promise
+ */
+uajax.upload = (path, formData, progress = false, progressBarID = '', { ...options } = {}) => {
+  // If uajax_debug is enabled, we'll output what the module is doing to the console.
+  const tx = 'color: rgb(0, 255, 145';
+  const rx = 'color: rgb(52, 164, 250)';
+
+  const initializedOptions = applyDefaultOptions(options);
+
+  if (uajaxDebug) {
+    console.log('POST (File Upload) %c>>>', tx, path, formData);
+  }
+
+  return new Promise((resolve) => {
+    $.ajax({
+      xhr: () => {
+        const xhr = new window.XMLHttpRequest();
+
+        xhr.upload.addEventListener(
+          'progress',
+          (evnt) => {
+            let progressPercentage = 0;
+
+            if (evnt.lengthComputable) {
+              progressPercentage = evnt.loaded / evnt.total;
+              progressPercentage = parseInt(progressPercentage * 100, 10);
+            }
+
+            if (progress) {
+              $(`#${progressBarID}`).css({ width: `${progressPercentage}%` });
+              $(`#${progressBarID}`).html(`${progressPercentage}%`);
+            }
+          },
+          false,
+        );
+
+        return xhr;
+      },
+
+      method: 'POST',
+      url: path,
+      data: formData,
+      cache: false,
+      contentType: false,
+      crossDomain: initializedOptions.crossDomain,
+      processData: false,
+
+      success: (r, textStatus, xhr) => {
+        const response = new UAJAXResponse(r, xhr.status);
+
+        if (uajaxDebug) {
+          console.log('POST (File Upload) %c<<<', rx, path, response);
+        }
+
+        if (progress) {
+          $(`#${progressBarID}`).html('Upload completed!');
+        }
+
+        resolve(response);
+      },
+    }).catch((xhr) => {
+      const errorDetails = {
+        status: xhr.status,
+        response: xhr.responseText,
+      };
+
+      if (uajaxDebug) {
+        console.error(`Failed to upload file to ${path}`, errorDetails);
+      }
+
+      const response = new UAJAXResponse(errorDetails.response, errorDetails.status);
+      resolve(response);
+    });
+  });
+};
